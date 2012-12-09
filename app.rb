1e713754b7fa2744a073311e1786bdac48ed2324
Jason Galuten <jason@galuten.com>
2012-12-09 11:40:14 -0800

    first commit

---
+++ app.rb
@@ -0,0 +1,34 @@
+require 'sinatra'
+require 'haml'
+require 'mechanize'
+
+get '/' do
+  @lyrics = []
+  haml :index
+end
+
+post '/' do
+  query = "lyrics site:metrolyrics.com #{params[:query]}"
+  agent = Mechanize::new
+  page = agent.get("http://www.google.com")
+  google_form = page.form('f')
+  google_form.q = query
+  page = agent.submit(google_form, google_form.buttons.first)
+  page = agent.get(page.at('#ires ol a').attribute('href'))
+  @title = page.title
+  chunks = page.at('#lyrics-body').to_html.split '<br>'
+  @lyrics = []
+  chunks.each do |chunk|
+    n = Nokogiri::HTML(chunk)
+    n.content.lines.each do |line|
+      if !line.strip.empty?
+        @lyrics << line.strip unless line.include? 'metrolyrics'
+      end
+    end
+  end
+  haml :index
+end
+
+get '/views/style.css' do
+  sass :style
+end
